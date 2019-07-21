1f4540fa35b39cdb75f845b8efe852f3a8c92926
Adrian Głąbik <46246127+edenlandpl@users.noreply.github.com>
2019-07-21 12:19:09 +0200

    Add files via upload

---
+++ addingTwo16BitsNumbers.asm
@@ -0,0 +1,98 @@
+// dodawanie liczb 16 bitowych
+link l1:ct
+link l2:rdm
+ 
+equ cs:r8
+equ ip:r9
+\\ rezerwowanie rejestrow
+equ pom1: r2
+equ pom2: r3
+equ pom3: r4
+equ pom4: r5
+equ pom5: r6
+equ pom6: r7
+equ pom7: r8
+ 
+accept cs:ABCDh
+accept ip:1234h
+\\ wpisanie wartosci pod konkretne adresy
+dw ACF04h:5678h	\\	ACF04 ABCD
+dw BF125h:9ABCh	\\	BF125 BCDE
+dw D1346h:789Ah 	\\	D1346 CDEF
+macro mov reg1, reg2:{or reg1, reg2, z;}
+ 
+{LOAD rm, z;}
+  
+{mov cs, ABCDh;}
+{mov ip, 1234h;}
+{mov pom1, cs;}
+{cjs nz, obliczadr;}
+{mov nil, pom1; oey; ewl;}
+{mov nil, pom2; oey;ewh;}
+{R; mov pom4, bus_d; cjp rdm, cp;}
+ 
+{mov cs, BCDEh;}
+{mov ip, 2345h;}
+{mov pom1, cs;}
+{cjs nz, obliczadr;}
+{mov nil, pom1; oey; ewl;}
+{mov nil, pom2; oey;ewh;}
+{R; mov pom5, bus_d; cjp rdm, cp;}
+ 
+{mov cs, CDEFh;}
+{mov ip, 3456h;}
+{mov pom1, cs;}
+{cjs nz, obliczadr;}
+{mov nil, pom1; oey; ewl;}
+{mov nil, pom2; oey;ewh;}
+{R; mov pom6, bus_d; cjp rdm, cp;} 
+ 
+
+{mov cs, ABCDh;}
+{mov ip, 1234h;}
+{mov pom1, cs;}
+{cjs nz, obliczadr;}
+{mov nil, pom1; oey; ewl;}
+{mov nil, pom2; oey;ewh;}
+{W; mov nil, pom6; oey; cjp rdm, cp;}
+ 
+{mov cs, BCDEh;}
+{mov ip, 2345h;}
+{mov pom1, cs;}
+{cjs nz, obliczadr;}
+{mov nil, pom1; oey; ewl;}
+{mov nil, pom2; oey;ewh;}
+{W; mov nil, pom4; oey; cjp rdm, cp;}
+
+{mov cs, CDEFh;}
+{mov ip, 3456h;}
+{mov pom1, cs;}
+{cjs nz, obliczadr;}
+{mov nil, pom1; oey; ewl;}
+{mov nil, pom2; oey;ewh;}
+{W; mov nil, pom5; oey; cjp rdm, cp;}
+
+{and pom4, pom4, 00F0h;}
+{xor pom4,0060h;}
+
+{add pom7, 0000h, rm,z;load rm_z,flags;}	\\ dodanie do pom1 pom1 + rq i zapoamietaj q pom1
+
+{end;}
+ \\ wyzeruj z pom4 pierwsza druga czwarta liczba and'em, xor'em sprawdz czy na 3 pozycji jest 7, jesli rm_z = 1 to obroc w lewo, 0 -w prawo
+obliczadr{load rm,z;}
+
+{load rm, z;}				\\ zerowanie rejestru znacznikow
+{xor pom2, pom2, pom2;}		\\ zerowanie pom2
+{mov pom1, cs;}				\\ z cs kopiuj do pom1
+{mov rq, ip;}
+{push nz,3;}				\\ petla 4 razy
+{sll pom1;}					\\ przesuniecie w lewo o 1 bit
+{sl.25 pom2;}				\\ zgranie z rm do pom2 i przesuniecie 0 1 bit w lewo pom2, wykonaj 4 razy, aby przesun
+{rfct;}						\\ koniec petli
+{add pom1, pom1, rq,z;load rm,flags;}	\\ dodanie do pom1 pom1 + rq i zapoamietaj q pom1
+{add pom2,pom2,z,rm_c;}					\\ 
+{crtn nz;}	
+\\{mov pom1, cs;}				\\ z cs kopiuj do pom1
+\\{mov rq, ip;}
+ 
+ 
